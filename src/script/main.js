29fc360dcd434f939d9b01c80de3bd2b54e7be9c
Valentina <valelore98@gmail.com>
2022-10-26 16:31:09 +0200

    MILESTONE 2: showed workers data in DOM

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,4 @@
-let workerArray = [
+const workerArray = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
@@ -6,36 +6,53 @@ let workerArray = [
     },{
         name: "Angela Caroll",
         role: "Chief Editor",
-        imageUrl: "angela-caroll-chief-editor.jpg"
+        imageName: "angela-caroll-chief-editor.jpg"
     },{
         name: "Walter Gordon",
         role: "Office Manager",
-        imageUrl: "walter-gordon-office-manager.jpg"
+        imageName: "walter-gordon-office-manager.jpg"
     },{
         name: "Angela Lopez",
         role: "Social Media Manager",
-        imageUrl: "angela-lopez-social-media-manager.jpg"
+        imageName: "angela-lopez-social-media-manager.jpg"
     },{
         name: "Scott Estrada",
         role: "Developer",
-        imageUrl: "scott-estrada-developer.jpg"
+        imageName: "scott-estrada-developer.jpg"
     },{
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        imageUrl: "barbara-ramos-graphic-designer.jpg"
+        imageName: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
-printWorkersData(workerArray)
+printWorkersData(workerArray);
+showWorkersInDom(workerArray);
 
-function printWorkersData(workers){
+function printWorkersData(workers) {
     workers.forEach(worker => {
         printWorkerData(worker);
     });
 }
 
-function printWorkerData(worker){
+function printWorkerData(worker) {
     for (const key in worker) {
-        console.log(key + ": " + worker[key])
+        console.log(key + ": " + worker[key]);
     }
+}
+
+function showWorkersNoImage(workers) {
+    workers.forEach((worker, i) => {
+        const div = document.createElement('div');
+        div.id = `worker-${i}`;
+        for (const key in worker) {
+            const p = document.createElement("p");
+            p.id = div.id + key;
+            p.class = key;
+            key == "name" ? p.innerHTML = `<b>${worker[key]}</b>` : p.innerHTML = worker[key];
+            div.appendChild(p);
+        }
+
+        document.getElementById("workers-data").appendChild(div);
+    });
 }
