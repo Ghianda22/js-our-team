f7f4afc3f018ecebda865d050431e57cd892a49a
Valentina <valelore98@gmail.com>
2022-10-26 15:33:33 +0200

    Created array of workers

---
+++ src/script/main.js
@@ -0,0 +1,28 @@
+let workers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        imageName: "wayne-barnett-founder-ceo.jpg"
+    },{
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        imageUrl: "angela-caroll-chief-editor.jpg"
+    },{
+        name: "Walter Gordon",
+        role: "Office Manager",
+        imageUrl: "walter-gordon-office-manager.jpg"
+    },{
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        imageUrl: "angela-lopez-social-media-manager.jpg"
+    },{
+        name: "Scott Estrada",
+        role: "Developer",
+        imageUrl: "scott-estrada-developer.jpg"
+    },{
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        imageUrl: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
